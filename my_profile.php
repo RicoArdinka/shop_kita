8ded3b393be4ba1ee977b40f8e1026e570c5616d
rico ardinka <ricosepta1014@gmail.com>
2023-02-24 13:33:15 +0700

    protect page my_profile and check conten menu

--- my_profile.php
+++ my_profile.php
@@ -1,3 +1,15 @@
+<?php
+    //protect page my profile agar tidak bisa diakses kecuali telah login
+    if($user_id){
+        $module = isset($_GET['module']) ? $_GET['module'] : false;
+        $action = isset($_GET['action']) ? $_GET['action'] : false;
+        $mode = isset($_GET['mode']) ? $_GET['mode'] : false;
+    }else{
+        header("location: ".BASE_URL."index.php?page=login");
+    }
+?>
+
+
 <div id="bg-page-profile">
 
     <div id="menu-profile">
@@ -25,6 +37,15 @@
     </div>
 
     <div id="profile-content">
-        Belum data 
+        <?php
+             
+            //cek apakah content halaman ada di folder module 
+            $file = "module/$module/$action.php";
+            if(file_exists($file)){
+                include_once($file);
+            }else{
+                echo "<h3>Maaf, halaman tidak ditemukan</h3>";
+            }
+        ?>
     </div>
 </div>
