ae90ff35b408b7d65a18854f4ca250f9a80ecd6f
rico ardinka <ricosepta1014@gmail.com>
2023-02-25 14:05:39 +0700

    add action update

--- module/kategori/action.php
+++ module/kategori/action.php
@@ -7,6 +7,11 @@
     $status = $_POST['status'];
     $button = $_POST['button'];
 
-    mysqli_query($koneksi, "INSERT INTO kategori (kategori, status) VALUES ('$kategori', '$status')");
+    if($button == "Add"){ //jika menambahkan data pertama kali di form
+        mysqli_query($koneksi, "INSERT INTO kategori (kategori, status) VALUES ('$kategori', '$status')");
+    } else if($button == "Update"){ // jika mengupdate di form
+        $kategori_id = $_GET['kategori_id'];
 
+        mysqli_query($koneksi, "UPDATE kategori SET kategori='$kategori',status='$status' WHERE kategori_id='$kategori_id'");
+    }
     header("location: ".BASE_URL."index.php?page=my_profile&module=kategori&action=list");
