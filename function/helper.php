d361e5215b781208af66f5af2a6b598d0079d51d
rico ardinka <ricosepta1014@gmail.com>
2023-02-27 21:47:39 +0700

    create function rupiah

--- function/helper.php
+++ function/helper.php
@@ -1,3 +1,8 @@
 <?php
 
     define("BASE_URL", "http://localhost/weshop/"); // constanta(nama, nilainya)
+
+    function rupiah($nilai = 0){ //untuk membuat fungsi rupiah
+        $string = "Rp." . number_format($nilai);
+        return $string;
+    }
