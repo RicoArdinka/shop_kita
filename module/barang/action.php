22c9a5dce1ad7e135c2479b8f6a7f70e9f77e593
rico ardinka <ricosepta1014@gmail.com>
2023-02-28 20:37:21 +0700

    add action update

--- module/barang/action.php
+++ module/barang/action.php
@@ -10,17 +10,27 @@
     $button = $_POST['button'];
     $harga = $_POST['harga'];
     $stok = $_POST['stok'];
+    $update_gambar = "";
 
-    $nama_file = $_FILES["file"]["name"];
-    move_uploaded_file($_FILES["file"]["tmp_name"], "../../images/barang/".$nama_file);
+    if(!empty($_FILES["file"]["name"])){
+        $nama_file = $_FILES["file"]["name"];
+        move_uploaded_file($_FILES["file"]["tmp_name"], "../../images/barang/".$nama_file);
+
+        $update_gambar = ", gambar='$nama_file'";
+    }
 
     if($button == "Add"){ //jika menambahkan data pertama kali di form
         mysqli_query($koneksi, "INSERT INTO barang (nama_barang, kategori_id, spesifikasi, gambar, harga, stok, status) 
                                             VALUES ('$nama_barang', '$kategori_id', '$spesifikasi', '$nama_file', '$harga', '$stok', '$status')");
     }
-    // else if($button == "Update"){ // jika mengupdate di form
-    //     $kategori_id = $_GET['kategori_id'];
+    else if($button == "Update"){ // jika mengupdate di form
+        $barang_id = $_GET['barang_id'];
 
-    //     mysqli_query($koneksi, "UPDATE kategori SET kategori='$kategori',status='$status' WHERE kategori_id='$kategori_id'");
-    // }
+        mysqli_query($koneksi, "UPDATE barang SET kategori_id='$kategori_id',
+                                                  nama_barang='$nama_barang',
+                                                  spesifikasi='$spesifikasi',
+                                                  harga='$harga',
+                                                  status='$status' 
+                                                  $update_gambar WHERE barang_id='$barang_id'");
+    }
     header("location: ".BASE_URL."index.php?page=my_profile&module=barang&action=list");
